3b9d76768ea1b6e7a0877cf5d26cc89cb0f74f1c
RepoRanger37 <karthikeyab2003@gmail.com>
2025-06-27 11:59:47 +0530

    Update start_nodes.sh

--- start_nodes.sh
+++ start_nodes.sh
@@ -6,12 +6,19 @@ source /opt/ros/humble/setup.bash
 source /root/microros_ws/install/local_setup.bash
 source /root/Precision_landing_aruco/install/setup.bash
 
-# Run nodes in background
-ros2 run ark cam &
-ros2 run ark fb &
-ros2 run ark aruco &
-ros2 run precision_land precision_land &
-ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 -b 921600
-# Wait so container stays alive
-wait
+# Start micro-ROS agent
+ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 -b 921600 &
+echo "Started micro_ros_agent"
+sleep 3  # wait 3 seconds for initialization
+
+# Start aruco tracker
+ros2 launch ark aruco_tracker.launch.py &
+echo "Started aruco_tracker"
+sleep 2  # wait 2 seconds
+
+# Start precision_land (runs in foreground)
+ros2 run precision_land precision_land
+
+echo "Nodes finished. Keeping container alive..."
+exec bash
 
